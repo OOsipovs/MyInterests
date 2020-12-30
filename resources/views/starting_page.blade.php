bec0f705ffa3299b08988442e923908758510954
Olegs <olegs.osipovs@gmail.com>
2020-12-30 18:40:00 +0200

    Frontend and login setup

---
+++ resources/views/starting_page.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('page_description')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Starting Page</div>
+
+                <div class="card-body">
+                    Welcome to my starting page!!!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
